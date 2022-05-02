b12620e8854f8de86ff177045028c8fe2eedbf3d
JawaraGordon <jawaragordon@icloud.com>
2022-05-02 17:48:50 -0400

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React from 'react';
+// import user from '../data/user';
+import Links from './Links';
 
-function About() {
+function About(props) {
+  // console.log(props)
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {/* {props.bio} === '' ? null : {props.bio} */}
+      {/* {props.bio !== '' ? <p>{props.bio}</p> : null} */}
+      {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links />
     </div>
   );
 }
